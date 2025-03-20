ac06fbbe2cd4a0a6ed94b376073259f50b04bb44
Niels Gandraß <niels@gandrass.de>
2025-03-20 15:45:13 +0100

    Implement basic plugin interface

---
+++ classes/archivingevent.php
@@ -0,0 +1,46 @@
+<?php
+
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Quiz activity archiving driver.
+ *
+ * @package     archivingmod_quiz
+ * @copyright   2025 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace archivingevent_apistub;
+
+use local_archiving\driver\archivingevent_base;
+
+// @codingStandardsIgnoreFile
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+
+/**
+ * Quiz activity archiving driver.
+ */
+class archivingevent extends archivingevent_base {
+
+    /**
+     * @inheritDoc archivingevent_base::get_name()
+     */
+    public static function get_name(): string {
+        return get_string('pluginname', 'archivingmod_quiz');
+    }
+
+}
